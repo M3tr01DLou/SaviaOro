f983ccc407f9837741e3a30903d15084ef834a8a
M3tr01DLou <loumaspa@hotmail.com>
2023-10-26 20:50:02 +0200

    Login button

--- SaviaOro.WEB/Program.cs
+++ SaviaOro.WEB/Program.cs
@@ -1,6 +1,3 @@
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -26,14 +23,5 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationProviderJWT>();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
 builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
-
-
-builder.Services
-	.AddBlazorise(options =>
-	{
-		options.Immediate = true;
-	})
-	.AddBootstrapProviders()
-	.AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
