f1368e3352bd9877015f934c19cd384c8ff6c89f
PANEL\luisjavier.diez <luisjavier.diez@panel.es>
2023-10-26 12:34:22 +0200

    Add Security WEB

--- SaviaOro.WEB/Program.cs
+++ SaviaOro.WEB/Program.cs
@@ -2,19 +2,28 @@ using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SaviaOro.WEB;
+using SaviaOro.WEB.Auth;
 using SaviaOro.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.lousoftware.eu/") });
+//TODO:
+//PROD
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.lousoftware.eu/") });
+
+//TEST
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7223/") });
+
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 builder.Services
 	.AddBlazorise(options =>
